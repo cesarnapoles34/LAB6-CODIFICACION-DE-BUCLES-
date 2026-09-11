03d1b76a10fa25dba84aec73519a04a31a2e6cf8
cesarnapoles34 <cesarnapoles34@gmail.com>
2026-09-11 12:26:34 -0600

    primer commit

---
+++ ejercicio1.py
@@ -0,0 +1,33 @@
+#Python es un lenguaje de alto nivel, orientado a objetos, lenguaje interpretado
+#Variables : Son espacios de memoria en los que voy a almacenar distintos tipos de datos 
+#Cuatro tipos de datos simples
+
+#string : cadena de texto, se escribe con "", '', """", '''''',
+#Las comillas simples y dobles se utilizan en textos de una sola linea
+#Las comillas triples se utilizan en textos multiples 
+
+nombre = "cesar"
+ch = "CH73"
+#Numeros enteros
+30
+
+#Numeros flotantes (punto decimal)
+30.5
+
+#Boleanos : Sedivide en True o False (se escribe con mayuscula)
+
+True 
+False 
+
+
+
+Saludo = "Hola " + nombre + " Bienvenida " + ch
+print(Saludo)
+
+#Ejercicio Input/almaceno datos/output
+
+nombre = input("¿Como te llamas? ")
+edad = input("¿Cuantos años tienes?: ")
+ch = input("¿Cual es tu ch?: ")
+dato_desconocido = input("Cuentame un dato muy secreto: ")
+dato_random = input("Cuentame un dato random: ")
---
+++ hello.js
@@ -0,0 +1,6 @@
+console.log("hello word");
+
+console.log("Soy Cesar");
+console.log("Estoy aprendiendo java");
+console.log("Quiero crear una app");
+
